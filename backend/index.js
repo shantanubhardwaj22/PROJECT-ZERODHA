7c9d5824bbabd15946dc8055fbb9f3c0e276510b
Shantanu Bhardwaj <shantanubhardwaj@Shantanus-MacBook-Air.local>
2026-01-05 00:58:13 +0530

    cleanup: remove node_modules and cache

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const { HoldingsModel } = require("./model/HoldingsModel");
 const authRoutes = require("./routes/auth");
 const bcrypt = require("bcryptjs");
